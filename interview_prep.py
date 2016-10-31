45e1699baa19980793cc383ad5f8f8ef47a9836e
Tarang <tarang.marathe@gmail.com>
2016-10-31 11:40:38 -0400

    stuff

--- interview_prep.py
+++ interview_prep.py
@@ -1,3 +1,5 @@
+import random
+import sys
 
 def matching_parentheses(str):
     c = 0
@@ -8,7 +10,26 @@ def matching_parentheses(str):
             c -= 1
     return c == 0
 
+# better than linear time using modified binary search
+def find_missing_no(lst, left=0, right=(len(lst)-1), length=len(lst)):
+    if right <= left+1:
+        return left+1
+    else:
+        mid = (left+right) // 2
+        if mid - left != lst[mid] - lst[left]:
+            return find_missing_no
+
+
+    
 
 if __name__ == '__main__':
-    inp = input("#>")
-    print(matching_parentheses(inp))
+    #inp = input("#>")
+    #print(matching_parentheses(inp))
+    
+
+
+    num_list = [x for x in range(101)]
+    del num_list[random.randint(0,99)]
+    print(num_list)
+    print(find_missing_no(num_list))
+    
