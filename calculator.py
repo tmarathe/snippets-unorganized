8bb3ef2a7f15f55c00775cc53ea13b26a31c2d73
Tarang <tarang.marathe@gmail.com>
2017-03-08 21:42:08 -0500

    started implementation of grammar rules

--- calculator.py
+++ calculator.py
@@ -6,15 +6,41 @@ import re
 
 # this implementation uses regular expressions for parsing as opposed to recursive descent
 
-token_map = {}
-token_map['+'] = 'ADD'
-token_map['-'] = 'SUB'
-token_map['*'] = 'MUL'
-token_map['/'] = 'DIV'
+tm = {}
+tm['+'] = 'ADD'
+tm['-'] = 'SUB'
+tm['*'] = 'MUL'
+tm['/'] = 'DIV'
 
+def expression(tokens):
+    pass
+
+def mult_div_term(tokens):
+    result, tokens = atom(tokens)
+
+    if tokens[0] == '*':
+        tokens = tokens[1:]
+        result *= mult_div_term(tokens)
+    elif tokens[0] == '/':
+        tokens = tokens[1:]
+        result /= mult_div_term(tokens)
+
+    return result, tokens
+
+
+def atom(tokens):
+    result = None
+    if tokens[0].isdigit():
+        result = float(tokens[0])
+    elif tokens[0] == '(':
+        result = expression(tokens[1:])
+    else:
+        return result, tokens
+    return result, tokens[1:]
 
 
 if __name__ == '__main__':
-    inp = input('Please enter a valid expression:')
 
-    add_sub_tokens = re.split('(\+|\-)', inp.replace(" ", ""))
+    inp = input('Please enter a valid expression:')
+    tokens = re.findall(r'[\d.]+|[\+\-*/()]', inp.replace(" ", ""))
+
